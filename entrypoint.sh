17f0deeaeecbcdebb3a975869256375456975795
example <example@example.com>
2025-04-19 02:20:30 -0400

    🔨 update entrypoint to log all events from queue process and webserver

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+# Start each process and redirect output to log files
+uv run manage.py runserver 0.0.0.0:80 > runserver.log 2>&1 & \
+uv run manage.py rss-episode-downloader > rss-episode-downloader.log 2>&1 & \
+uv run manage.py rss-subscriptions-refresher > rss-subscriptions-refresher.log 2>&1 &
 
-python manage.py runserver 0.0.0.0:8000
+# Use tail to follow the logs
+tail -f runserver.log rss-episode-downloader.log rss-subscriptions-refresher.log
