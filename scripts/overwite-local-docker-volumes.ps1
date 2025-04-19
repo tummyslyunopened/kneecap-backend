145dd5bd86502c005b381004b0817c26c186ba83
example <example@example.com>
2025-04-19 09:52:01 -0400

    update volume copy logic

--- scripts/overwite-local-docker-volumes.ps1
+++ scripts/overwite-local-docker-volumes.ps1
@@ -1,5 +1,13 @@
 docker run -d -v kneecap_db:/db/ -v kneecap_static:/static/ -v kneecap_media:/media/ kneecap/server:0.0
 $containerId = $(docker ps -lq)
+
+# // Clear the static and media directories
+docker exec ${containerId} sh -c "rm -rf /static/*"
+docker exec ${containerId} sh -c "rm -rf /media/*"
+
+# // Recreate the static and media directories
+docker exec ${containerId} sh -c "mkdir -p /static /media"
+
 docker cp ../db.sqlite3 ${containerId}:/db/
 Get-ChildItem -Path ../static | ForEach-Object {
     docker cp $_.FullName ${containerId}:/static/
