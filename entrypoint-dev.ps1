f0cf150d89dac2ddc4f3b1eb8c48c0be3575af41
example <example@example.com>
2025-04-19 09:36:19 -0400

    move logs to logs folder

--- entrypoint-dev.ps1
+++ entrypoint-dev.ps1
@@ -1,12 +1,7 @@
-# Create log files if they do not exist
-if (-not (Test-Path "runserver.log")) { New-Item "runserver.log" -ItemType File }
-if (-not (Test-Path "rss-episode-downloader.log")) { New-Item "rss-episode-downloader.log" -ItemType File }
-if (-not (Test-Path "rss-subscriptions-refresher.log")) { New-Item "rss-subscriptions-refresher.log" -ItemType File }
-
 # Start each process and redirect output and error to separate log files
-Start-Process -FilePath "uv" -ArgumentList "run manage.py runserver 0.0.0.0:80" -RedirectStandardOutput "runserver-output.log" -RedirectStandardError "runserver-error.log" -NoNewWindow
-Start-Process -FilePath "uv" -ArgumentList "run manage.py rss-episode-downloader" -RedirectStandardOutput "rss-episode-downloader-output.log" -RedirectStandardError "rss-episode-downloader-error.log" -NoNewWindow
-Start-Process -FilePath "uv" -ArgumentList "run manage.py rss-subscriptions-refresher" -RedirectStandardOutput "rss-subscriptions-refresher-output.log" -RedirectStandardError "rss-subscriptions-refresher-error.log" -NoNewWindow
+Start-Process -FilePath "uv" -ArgumentList "run manage.py runserver 0.0.0.0:80" -RedirectStandardOutput "./logs/runserver-output.log" -RedirectStandardError "./logs/runserver-error.log" -NoNewWindow
+Start-Process -FilePath "uv" -ArgumentList "run manage.py rss-episode-downloader" -RedirectStandardOutput "./logs/rss-episode-downloader-output.log" -RedirectStandardError "./logs/rss-episode-downloader-error.log" -NoNewWindow
+Start-Process -FilePath "uv" -ArgumentList "run manage.py rss-subscriptions-refresher" -RedirectStandardOutput "./logs/rss-subscriptions-refresher-output.log" -RedirectStandardError "./logs/rss-subscriptions-refresher-error.log" -NoNewWindow
 
 # Use Get-Content to follow the logs
-Get-Content runserver-output.log, runserver-error.log, rss-episode-downloader-output.log, rss-episode-downloader-error.log, rss-subscriptions-refresher-output.log, rss-subscriptions-refresher-error.log -Wait
+Get-Content ./logs/runserver-output.log, ./logs/runserver-error.log, ./logs/rss-episode-downloader-output.log, ./logs/rss-episode-downloader-error.log, ./logs/rss-subscriptions-refresher-output.log, ./logs/rss-subscriptions-refresher-error.log -Wait
