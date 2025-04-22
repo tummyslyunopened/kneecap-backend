c0bb9e729d4b6ff8bc413adbda6fe888633063ab
example <example@example.com>
2025-04-22 06:56:07 -0400

    fix failing entrypoint script migrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,8 @@
 # Create logs directory if it doesn't exist
 mkdir -p ./logs
 
-uv sync 
-uv run makemigrations
-uv run migrate
+uv run manage.py makemigrations
+uv run manage.py migrate
 
 # Start each process in the background and redirect output and error to separate log files
 uv run manage.py runserver 0.0.0.0:80 > ./logs/runserver-output.log 2> ./logs/runserver-error.log &
