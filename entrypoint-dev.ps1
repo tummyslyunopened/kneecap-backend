4d369e9dc67a5ca362d6cf399ff03b27fdcc4abf
example <example@example.com>
2025-04-27 12:34:11 -0400

    update dev entrypoint to latest format

--- entrypoint-dev.ps1
+++ entrypoint-dev.ps1
@@ -1,12 +1,15 @@
+# Create logs directory if it doesn't exist
+if (!(Test-Path -Path './logs')) {
+    New-Item -ItemType Directory -Path './logs' | Out-Null
+}
+
 # Run migrations before starting runtime processes
 uv run manage.py makemigrations
 uv run manage.py migrate
 
 # Start each process and redirect output and error to separate log files
 Start-Process -FilePath "uv" -ArgumentList "run manage.py runserver 0.0.0.0:80" -RedirectStandardOutput "./logs/runserver-output.log" -RedirectStandardError "./logs/runserver-error.log" -NoNewWindow
-Start-Process -FilePath "uv" -ArgumentList "run manage.py rss-episode-downloader" -RedirectStandardOutput "./logs/rss-episode-downloader-output.log" -RedirectStandardError "./logs/rss-episode-downloader-error.log" -NoNewWindow
-Start-Process -FilePath "uv" -ArgumentList "run manage.py rss-subscriptions-refresher" -RedirectStandardOutput "./logs/rss-subscriptions-refresher-output.log" -RedirectStandardError "./logs/rss-subscriptions-refresher-error.log" -NoNewWindow
-Start-Process -FilePath "uv" -ArgumentList "run manage.py rss-q-subscriptions" -RedirectStandardOutput "./logs/rss-q-subscriptions-output.log" -RedirectStandardError "./logs/rss-q-subscriptions-error.log" -NoNewWindow
+Start-Process -FilePath "uv" -ArgumentList "run manage.py sync_rss_data" -RedirectStandardOutput "./logs/sync-rss-data-output.log" -RedirectStandardError "./logs/sync-rss-data-error.log" -NoNewWindow
 
 # Use Get-Content to follow the logs
-Get-Content ./logs/runserver-output.log, ./logs/runserver-error.log, ./logs/rss-episode-downloader-output.log, ./logs/rss-episode-downloader-error.log, ./logs/rss-subscriptions-refresher-output.log, ./logs/rss-subscriptions-refresher-error.log, ./logs/rss-q-subscriptions-output.log, ./logs/rss-q-subscriptions-error.log -Wait
+Get-Content ./logs/runserver-output.log, ./logs/runserver-error.log, ./logs/sync-rss-data-output.log, ./logs/sync-rss-data-error.log -Wait
